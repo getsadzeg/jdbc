f0756000c063cb3a09d83dbcb8aaf6cde9c79c80
getsadzeg <getsadzeguram@gmail.com>
2016-06-06 21:08:20 +0400

    finally got true on running statement

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         ConnectDB db = new ConnectDB("jdbc:mysql://192.168.1.2:3306", "root", "root");
         System.out.println(db.openConnection());
-        db.setUrl("jdbc:mysql://192.168.1.2:3306/areyoual_army");
-        System.out.println(db.runStatement("SELECT Id, Name, Status, Message, Longitude, Latitude FROM soldiers"));
+        
+        System.out.println(db.runStatement("SELECT Id, Name, Status, Message, Longitude, Latitude FROM areyoual_army.soldiers"));
         
         System.out.println(db.closeConnection());
     }
